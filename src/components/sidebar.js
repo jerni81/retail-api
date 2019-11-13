f4bc071353553213aa7d866c3f180774e3374609
jerni81 <jerni81@yahoo.com>
2019-11-13 12:30:52 -0500

    api call to mockaroo

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
+import axios from 'axios';
 
 function SideBar() {
+
+const [data, setData] = useState({})
+
+const getData = async() => {
+  const data = await axios.get('https://my.api.mockaroo.com/retail.json?key=b31b35d0')
+  console.log(data, 'this is data');
+  setData(data)
+  return data
+}
+
+useEffect(()=>{
+})
+
+const getThis = () => {
+  getData()
+}
+
+const getThat = () => {
+  console.log(data);
+}
+
   return (
+
     <div className="sidebar">
+      <button onClick={getThis}>This</button>
+      <button onClick={getThat}>That</button>
       <ul>
         <li>This</li>
         <li>That</li>
