c45892a18458041b46a689a5e5de23f406499ab9
jerni81 <jerni81@yahoo.com>
2020-01-09 13:58:16 -0500

    updated comments

--- src/components/main.js
+++ src/components/main.js
@@ -10,11 +10,13 @@ function Main() {
   const [japan, setJapan] = useState("");
   const [selected, setSelected] = useState({ name: "" });
 
-  // API call
+  // Axios API call to render data into different categories
   const getData = async () => {
     const data = await axios.get(
       "https://cors-anywhere.herokuapp.com/https://whiskyhunter.net/api/distilleries_info/"
     );
+    // maps data into objects and seperates them by country, then uses a
+    // spread operator to add them to the correct array
     const list = data.data.map((d, i) => {
       if (d.country === "Japan") {
         let jDistillery = {
@@ -41,6 +43,9 @@ function Main() {
     return list;
   };
 
+  // this is the logic that determines is the API call needs to be made
+  // and if so calls it, it then updates state for go to indicate the
+  // call has been made
   useEffect(() => {
     if (go.go === true) {
       getData();
@@ -49,6 +54,10 @@ function Main() {
   }, [go.go]);
 
   // Rendering List in View
+
+  // This checks to see if japan is empty, to see if the API call has
+  // returned any data yet, if not it send a message to view asking the
+  //to "Please wait", if so it directs the user to "make a choice"
   const shwList = () => {
     if (japan === "") {
       return (
@@ -64,21 +73,30 @@ function Main() {
         </div>
       );
     }
+
+    // This logic checks to see if the the state of show has been been
+    // updated  to either "japan" or "scotland" so as to render the correct
+    // list in the view component
     if (show.show === "japan") {
       return (
         japan &&
         japan.map((d, i) => {
           return (
+            // this is a ternary statement to dynamically render the className
+            // for each div created in the map function
             <div
               className={selected.name === d.name ? "selectedItem" : "listItem safari_only" }
               key={i}
             >
               <h3>{d.name}</h3>
               <h5>{d.country}</h5>
+              // this shows more info on whiskies while item is selected
               <div className={selected.name === d.name? "show" : "noShow"}>
                 <b>Rating: {d.rating}</b>
                 <b># of Whiskies: {d.whiskies}</b>
               </div>
+              // this onClick changes the state of selectes to dynamically
+              // render the className which creates an individual item view
               <button onClick={(event) => shwSelected(event, d.name)}>{selected.name === d.name ? "Go Back" : "Select" }</button>
             </div>
           );
@@ -90,16 +108,21 @@ function Main() {
         scotland &&
         scotland.map((d, i) => {
           return (
+            // this is a ternary statement to dynamically render the className
+            // for each div created in the map function
             <div
               className={selected.name === d.name ? "selectedItem" : "listItem safari_only" }
               key={i}
             >
               <h3>{d.name}</h3>
               <h5>{d.country}</h5>
+              // this shows more info on whiskies while item is selected
               <div className={selected.name === d.name ? "show" : "noShow"}>
                 <b>Rating: {d.rating}</b>
                 <b># of Whiskies: {d.whiskies}</b>
               </div>
+              // this onClick changes the state of selectes to dynamically
+              // render the className which creates an individual item view
               <button onClick={(event) => shwSelected(event, d.name)}>{selected.name === d.name ? "Go Back" : "Select" }</button>
             </div>
           );
@@ -108,6 +131,7 @@ function Main() {
     }
   };
 
+  // These are the functions passed to SideBar to create the onClick events
   const shwJapan = () => {
     setShow({ show: "japan" });
   };
@@ -115,7 +139,8 @@ function Main() {
     setShow({ show: "scotland" });
   };
 
-  // Rendering selected items
+  // This is the function that sets the selected name to create an individual
+  //view for the item
   const shwSelected = (event, d) => {
     event.preventDefault();
     if (selected.name === "") {
